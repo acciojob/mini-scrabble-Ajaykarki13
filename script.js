6099523b9d7d2cfdc54090073c2be4d8350543bf
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-01-12 17:29:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+let input = document.getElementById("evaluatedText");
+let counter = document.getElementById("letterCount");
+
+input.addEventListener("input",countletters)
+
+function countletters()
+{
+    counter.innerHTML=input.value.length;
+}
